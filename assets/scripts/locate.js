a03d741a158457912dca70c2d9bc3dbadb1bf37a
Michael Northorp <mike.northorp@gmail.com>
2013-06-20 20:43:10 -0700

    Split up functions refactoring

--- assets/scripts/locate.js
+++ assets/scripts/locate.js
@@ -211,28 +211,3 @@ window.setInterval(function() {
     document.getElementById("soldierCount").innerHTML = "Soldier Count " + soldierCount;
   }
 }, 3000);
-
-
-/* This function submits username and player team */
-function Submit() {
-  var name = document.getElementById('name').value;
-
-  if (name != null)
-    var profile_name = name;
-
-  document.getElementById('NAME').innerHTML = "Name: " + profile_name;
-
-  var factions = document.getElementById('profile').elements['faction'];
-  var factions_list = '';
-
-  for (i = 0; i < factions.length; i++) {
-    if (factions[i].checked) {
-      factions_list += factions[i].value;
-    }
-  }
-  document.getElementById('FACTION').innerHTML = "Faction: " + factions_list;
-  document.getElementById('profile').innerHTML = "";
-
-  // Call google map setup
-  setUpMap();
-}
