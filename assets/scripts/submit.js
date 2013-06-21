ea42649ce5a1c1657e08651c6ab7d59507101592
Michael Northorp <mike.northorp@gmail.com>
2013-06-20 22:29:30 -0700

    Modifed submit.js to use jquery and refactored some on index.html

--- assets/scripts/submit.js
+++ assets/scripts/submit.js
@@ -1,23 +1,27 @@
 /* This function submits username and player team */
-function Submit() {
-  var name = document.getElementById('name').value;
+$(document).ready(function() {
+  $("#factionSubmit").click(function() {
+    var name = document.getElementById('name').value;
 
-  if (name != null)
-    var profile_name = name;
-
-  document.getElementById('NAME').innerHTML = "Name: " + profile_name;
-
-  var factions = document.getElementById('profile').elements['faction'];
-  var factions_list = '';
-
-  for (i = 0; i < factions.length; i++) {
-    if (factions[i].checked) {
-      factions_list += factions[i].value;
+    if (name != null) {
+      var profile_name = name;
     }
-  }
-  document.getElementById('FACTION').innerHTML = "Faction: " + factions_list;
-  document.getElementById('profile').innerHTML = "";
 
-  // Call google map setup
-  setUpMap();
-}
+    document.getElementById('NAME').innerHTML = "Name: " + profile_name;
+
+    var factions = document.getElementById('profile').elements['faction'];
+    var factions_list = '';
+
+    for (i = 0; i < factions.length; i++) {
+      if (factions[i].checked) {
+        factions_list += factions[i].value;
+      }
+    }
+
+    document.getElementById('FACTION').innerHTML = "Faction: " + factions_list;
+    document.getElementById('profile').innerHTML = "";
+
+    // Call google map setup
+    setUpMap();
+  });
+});
